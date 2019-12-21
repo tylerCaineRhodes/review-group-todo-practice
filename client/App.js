b672f689c8703fd90e5e4e0f81fef4638179eff1
Rob Peschke <rspesc31@gmail.com>
2019-12-20 20:40:03 -0600

    new additions

--- client/App.js
+++ client/App.js
@@ -1,15 +1,9 @@
 import React from "react";
-import Axios from "axios";
-
 export default class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  
-
   render() {
     return (
       <div>
-        Hey there killer
+        Hello World!
       </div>
     );
   }
