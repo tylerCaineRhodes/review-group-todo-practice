9e798a735423bf7812fe901d0c13f9c3fc57e89f
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-01-01 17:46:35 -0600

    Add render function to class

--- client/App.js
+++ client/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-    render(){
-           
-    }
+    this.state = {};
+   };
+
+   render(){
+       return (
+           <div>
+               <h1>Hello!<h1/>
+            <div/>
+       )
+   }
+
 
 
 }
+
 export default App;
