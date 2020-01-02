9e5b6a1e1a3302e41c4ca8d864cb50005745bfbf
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-01-01 19:07:16 -0600

    Create basic skeleton for App

--- client/App.js
+++ client/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
-export default class App extends React.Component {
+ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {};
+    this.state = {groceries: ['bananas', 'cigars', 'bowl of snakes']};
    };
 
    render(){
        return (
-           `<div>
-             <h1>Hello!<h1/>
-            <div/>`
+           <div>
+             <h1>Hello!</h1>
+            </div>
        );
    }
 
 }
+
+export default App
