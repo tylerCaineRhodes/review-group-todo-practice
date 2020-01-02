ed9da5bbfab7664078209017b561a7a78b23a7b3
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-01-01 18:56:50 -0600

    Get something to render on screen

--- client/App.js
+++ client/App.js
@@ -9,10 +9,10 @@ export default class App extends React.Component {
 
    render(){
        return (
-           <div>
-               <h1>Hello!<h1/>
-            <div/>
-       )
+           `<div>
+             <h1>Hello!<h1/>
+            <div/>`
+       );
    }
 
 }
